9c439c8135d2e55c66ac9c057c186c5082046af3
webdevjosh <ozzikid@gmail.com>
2020-06-29 10:46:18 -0400

    updated to use a file write to check for new items instead of a DB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Twitter = require('twitter')
 const config = require('./utils/config')
 const axios = require('axios')
 const convert = require('xml-js')
+const fs = require('fs')
 
 var client = new Twitter({
     consumer_key: config.TWITTER_CONSUMER_KEY,
@@ -12,23 +13,33 @@ var client = new Twitter({
 
 const tweet_em = async (feed_url) => {
     const xml = await axios.get(feed_url)
-    const converted = convert.xml2js(xml.data, { compact: true, spaces: 4 })
-    const elements = converted.rss.channel.item
+    const oldXml = fs.readFileSync('nbxml.xml', { encoding: 'utf8', flag: 'r' })
+    if (oldXml === xml.data) {
+        return
+    }
+    fs.writeFileSync('nbxml.xml', xml.data)
 
-    elements.forEach(async elem => {
-        const elem_title = elem.title._text
-        const elem_link = elem.link._text
-        const tiny_url = await axios.get(`http://tinyurl.com/api-create.php?url=${elem_link}`)
-        const twitter_link = tiny_url.data
-        const status = `${elem_title} ${twitter_link}`
-        client.post('statuses/update', { status: status }, (error, tweet, response) => {
-            console.log(tweet)
-            if (!error) {
+    let converted = convert.xml2js(xml.data, { compact: true, spaces: 4 })
+    const newElements = converted.rss.channel.item
+    converted = convert.xml2js(oldXml, { compact: true, spaces: 4 })
+    const oldElements = converted.rss.channel.item
 
+    newElements.forEach(async elem => {
+        if (!oldElements.find(e => e.title._text === elem.title._text)) {
+            const elem_title = elem.title._text
+            const elem_link = elem.link._text
+            const tiny_url = await axios.get(`http://tinyurl.com/api-create.php?url=${elem_link}`)
+            const twitter_link = tiny_url.data
+            const status = `${elem_title} ${twitter_link}`
+            client.post('statuses/update', { status: status }, (error, tweet, response) => {
                 console.log(tweet)
-            }
-        })
+                if (!error) {
 
+                    console.log(tweet)
+                }
+            })
+        }
     })
 }
+// tweet_em('https://www.newsbusters.org/autotweet/business')
 tweet_em('https://www.newsbusters.org/feed/twitter')
